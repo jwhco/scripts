2e9d3239271cea54f17509c758198e4e1d310684
Justin Hitt <justin@jwhco.org>
2022-12-21 18:53:34 -0500

    Script pandoc to docx to markdown

---
+++ Text2Markdown/Docx2Markdown.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+#
+# Convert from DocX to Markdown
+#
+# Purpose: Receive a filename, check docx, convert.
+# Platform: Git Bash
+# Author: Justin Hitt
+# Usage: Only works with DOCX files.
+#
+#   find . -name "*.docx" -type f -print | while read file; do $0 "$file" ; done
+#
+
+echo DEBUG: START
+# Allow space in filename
+OIFS="$IFS"
+
+# Get input filename
+InputFile="$1"
+if [ ! -f "$InputFile" ]
+then
+  echo "File Not Found"
+  exit;
+fi
+echo DEBUG: InputFile = "$InputFile"
+
+# Extract Root Path
+FullPath=`readlink -f "$InputFile" `
+RootPath=`dirname "$FullPath" `
+echo DEBUG: FullPath = "$FullPath"
+echo DEBUG: RootPath = "$RootPath"
+
+# Build output file name
+BaseFile=`basename "$InputFile" `
+NewFile="${BaseFile%.*}".md
+echo DEBUG: BaseFile = "$BaseFile"
+echo DEBUG: NewFile = "$NewFile"
+
+OutputFile="$RootPath/$NewFile"
+echo DEBUG: OutputFile = "$OutputFile"
+
+# START: Use Pandoc to DocX to Markdown
+pandoc -s "$FullPath" -t markdown -o "$OutputFile"
+
+# Reset values
+IFS="$OIFS"
+
+echo DEBUG: END
+
+###
